09be161715e38958bfc0743c7ab6f48ce23623db
Kabir Singh <singhkabir44778@gmail.com>
2026-03-18 16:50:11 +0530

    Revert "feat : create pip component"

--- examples/react-demo/src/App.tsx
+++ examples/react-demo/src/App.tsx
@@ -1,6 +1,9 @@
 import { useEffect } from 'react'
-import { AppStorys, Banner,Story , Pip } from '@appstorys/react-web'
+import { AppStorys, Banner, Story } from '@appstorys/react-web'
+import Floater from '../../../src/components/Floater';
 import './App.css'
+import fetchAllCampaigns from '../../../src/core/fetchAllCampaigns';
+import useAppStorysStore from '../../../src/core/store';
 
 function App() {
   const DEMO_SCREEN_NAME = 'Home Screen';
@@ -9,6 +12,10 @@ function App() {
     const initSDK = async () => {
       try {
         console.log('[App] Initializing AppStorys SDK...');
+        // Fetch and store all campaigns before initializing SDK
+        const { campaigns } = await fetchAllCampaigns('12a9eac5-94ee-4735-9aa6-b8a94cb8fbbb');
+        useAppStorysStore.getState().saveAllCampaigns(campaigns);
+        console.log('[App] All campaigns fetched and saved:', campaigns);
         await AppStorys.initialize({
           appId: 'f69bdccf-b20f-4938-b39e-7075d76db791',
           accountId: '12a9eac5-94ee-4735-9aa6-b8a94cb8fbbb',
@@ -35,7 +42,7 @@ function App() {
 
       <Story />
       <Banner />
-      <Pip />
+      <Floater />
 
       <div className="card">
         <button onClick={() => AppStorys.trackEvent('button_click')}>
